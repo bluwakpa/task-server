dd31c894c5c0f0483a24b143bd612067bc238196
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-04-26 17:52:52 -0700

    db ssl

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const { PORT, DATABASE_URL } = require('./config')
 const db = knex({
   client: 'pg',
   connection: `${DATABASE_URL}?sslmode=require`,
+  ssl: { rejectUnauthorized: false }
 })
 
 app.set('db', db)
