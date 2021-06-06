f4087eb51eb8e08515c676dc3f83295ed4f9765d
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-06-06 16:07:19 -0700

    cors

--- src/app.js
+++ src/app.js
@@ -15,9 +15,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
